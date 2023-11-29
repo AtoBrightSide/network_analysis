d5845cb1a2d3980fedc42220d1d1bdfec537732e
Abel Mesele <se.abel.mesele@gmail.com>
2023-11-29 16:25:31 +0300

    Load json in slack_parser

--- src/loader.py
+++ src/loader.py
@@ -85,58 +85,59 @@ class SlackDataLoader:
             6. reset the index and return dataframe
         """
 
-        # specify path to get json files
         combined = []
-        for json_file in glob.glob(f"{path_channel}*.json"):
-            with open(json_file, 'r', encoding="utf8") as slack_data:
-                combined.append(slack_data)
-
-        # loop through all json files and extract required informations
         dflist = []
-        for slack_data in combined:
 
-            msg_type, msg_content, sender_id, time_msg, msg_dist, time_thread_st, reply_users, \
-                reply_count, reply_users_count, tm_thread_end = [], [], [], [], [], [], [], [], [], []
+        for json_file in glob.glob(f"{path_channel}*.json"):
+            with open(json_file, 'r', encoding="utf8") as file:
+                data = json.load(file)
+                combined.append(data)
 
-            for row in slack_data:
-                if 'bot_id' in row.keys():
-                    continue
-                else:
-                    msg_type.append(row['type'])
-                    msg_content.append(row['text'])
-                    if 'user_profile' in row.keys():
-                        sender_id.append(row['user_profile']['real_name'])
-                    else:
-                        sender_id.append('Not provided')
-                    time_msg.append(row['ts'])
-                    if 'blocks' in row.keys() and len(row['blocks'][0]['elements'][0]['elements']) != 0 :
-                        msg_dist.append(row['blocks'][0]['elements'][0]['elements'][0]['type'])
-                    else:
-                        msg_dist.append('reshared')
-                    if 'thread_ts' in row.keys():
-                        time_thread_st.append(row['thread_ts'])
-                    else:
-                        time_thread_st.append(0)
-                    if 'reply_users' in row.keys():
-                        reply_users.append(",".join(row['reply_users']))
-                    else:
-                        reply_users.append(0)
-                    if 'reply_count' in row.keys():
-                        reply_count.append(row['reply_count'])
-                        reply_users_count.append(row['reply_users_count'])
-                        tm_thread_end.append(row['latest_reply'])
-                    else:
-                        reply_count.append(0)
-                        reply_users_count.append(0)
-                        tm_thread_end.append(0)
-            data = zip(msg_type, msg_content, sender_id, time_msg, msg_dist, time_thread_st,
-                       reply_count, reply_users_count, reply_users, tm_thread_end)
-            columns = ['msg_type', 'msg_content', 'sender_name', 'msg_sent_time', 'msg_dist_type',
-                       'time_thread_start', 'reply_count', 'reply_users_count', 'reply_users', 'tm_thread_end']
+            # loop through all json files and extract required informations
+            for slack_data in combined:
 
-            df = pd.DataFrame(data=data, columns=columns)
-            df = df[df['sender_name'] != 'Not provided']
-            dflist.append(df)
+                msg_type, msg_content, sender_id, time_msg, msg_dist, time_thread_st, reply_users, \
+                    reply_count, reply_users_count, tm_thread_end = [], [], [], [], [], [], [], [], [], []
+
+                for row in slack_data:
+                    if 'bot_id' in row.keys():
+                        continue
+                    else:
+                        msg_type.append(row['type'])
+                        msg_content.append(row['text'])
+                        if 'user_profile' in row.keys():
+                            sender_id.append(row['user_profile']['real_name'])
+                        else:
+                            sender_id.append('Not provided')
+                        time_msg.append(row['ts'])
+                        if 'blocks' in row.keys() and len(row['blocks'][0]['elements'][0]['elements']) != 0 :
+                            msg_dist.append(row['blocks'][0]['elements'][0]['elements'][0]['type'])
+                        else:
+                            msg_dist.append('reshared')
+                        if 'thread_ts' in row.keys():
+                            time_thread_st.append(row['thread_ts'])
+                        else:
+                            time_thread_st.append(0)
+                        if 'reply_users' in row.keys():
+                            reply_users.append(",".join(row['reply_users']))
+                        else:
+                            reply_users.append(0)
+                        if 'reply_count' in row.keys():
+                            reply_count.append(row['reply_count'])
+                            reply_users_count.append(row['reply_users_count'])
+                            tm_thread_end.append(row['latest_reply'])
+                        else:
+                            reply_count.append(0)
+                            reply_users_count.append(0)
+                            tm_thread_end.append(0)
+                data = zip(msg_type, msg_content, sender_id, time_msg, msg_dist, time_thread_st,
+                           reply_count, reply_users_count, reply_users, tm_thread_end)
+                columns = ['msg_type', 'msg_content', 'sender_name', 'msg_sent_time', 'msg_dist_type',
+                           'time_thread_start', 'reply_count', 'reply_users_count', 'reply_users', 'tm_thread_end']
+
+                df = pd.DataFrame(data=data, columns=columns)
+                df = df[df['sender_name'] != 'Not provided']
+                dflist.append(df)
 
         dfall = pd.concat(dflist, ignore_index=True)
         dfall['channel'] = path_channel.split('/')[-1].split('.')[0]
